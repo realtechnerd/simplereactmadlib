2ffbcb6fc8da9b747322f54f034a4474550f2642
realtechnerd <40891074+realtechnerd@users.noreply.github.com>
2020-09-03 15:42:44 -0400

    Updates

---
+++ precache-manifest.ccacbeaff5e03eb3da04999a07514207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23885ed1d4632ab7664c0efadf39bb1",
+    "url": "/simplereactmadlib/index.html"
+  },
+  {
+    "revision": "c44df193b765150dc894",
+    "url": "/simplereactmadlib/static/css/main.39374ede.chunk.css"
+  },
+  {
+    "revision": "bb7eceef03a6e6d67732",
+    "url": "/simplereactmadlib/static/js/2.905b974a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simplereactmadlib/static/js/2.905b974a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c44df193b765150dc894",
+    "url": "/simplereactmadlib/static/js/main.313c6237.chunk.js"
+  },
+  {
+    "revision": "8cc900291b54557fd63b",
+    "url": "/simplereactmadlib/static/js/runtime-main.3822b20d.js"
+  }
+]);
